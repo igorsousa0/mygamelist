4a549feab2d6481658535d00ae5278e969ee8f4d
igorsousa0 <igorsousabr3@gmail.com>
2022-02-20 15:02:29 -0300

    Versão 1.0 - Correção

--- lib/config.dart
+++ lib/config.dart
@@ -8,6 +8,4 @@ var kTextColor = const Color.fromARGB(255, 56, 56, 56);
 var textFieldColor = const Color(0xFF2A2D3E);
 var kAppBarColor = const Color.fromARGB(255, 199, 199, 199);
 var iconSearchColor = const Color.fromARGB(255, 35, 83, 243);
-
-
 const defaultPadding = 16.0;
